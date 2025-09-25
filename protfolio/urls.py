d2a240cb2a7ca105e7c5be3ac1b011ed95ca25b8
Rinsin-P <Hrabu1234@gmail.com>
2025-09-26 01:47:07 +0530

    Add favicon

--- protfolio/urls.py
+++ protfolio/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', include('protapp.urls')),
+    # Favicon redirect
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'assets/img/favicon.png')),
 ]
 
 # Serve static files in development and production
